e54a559a1febd900e73b06e70266406ed6b117e0
Jonathan Horvath <byteme@bytedreamer.com>
2020-08-15 16:40:20 -0400

    Add input configuration page

--- src/Aporta.Shared/Calls/Paths.cs
+++ src/Aporta.Shared/Calls/Paths.cs
@@ -5,5 +5,6 @@ namespace Aporta.Shared.Calls
         public const string Extensions = "api/Extensions";
         public const string ExtensionPerformAction = "api/Extensions/{0}/action/{1}";
         public const string Outputs = "api/Outputs";
+        public const string Inputs = "api/Inputs";
     }
 }
