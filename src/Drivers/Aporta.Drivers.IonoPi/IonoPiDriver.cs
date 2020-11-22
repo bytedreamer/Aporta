a6f10f717aff89bc867720350ae0e1ec71f1bda2
Jonathan Horvath <byteme@bytedreamer.com>
2020-11-22 18:10:41 -0500

    Monitor Iono control points

--- src/Drivers/Aporta.Drivers.IonoPi/IonoPiDriver.cs
+++ src/Drivers/Aporta.Drivers.IonoPi/IonoPiDriver.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Device.Gpio;
+using System.Linq;
 using System.Threading.Tasks;
 using Aporta.Extensions.Endpoint;
 using Aporta.Extensions.Hardware;
@@ -15,6 +16,7 @@ namespace Aporta.Drivers.IonoPi
     {
         private GpioController _controller;
         private readonly List<IEndpoint> _endpoints = new List<IEndpoint>();
+        private readonly int[] _relayPins = {17, 27, 22, 23};
 
         public Guid Id => Guid.Parse("3803BA97-C8E3-479D-993B-E76DAB9ABED6");
 
@@ -27,17 +29,30 @@ namespace Aporta.Drivers.IonoPi
             var logger = loggerFactory.CreateLogger<IonoPiDriver>();
             
             _controller = new GpioController();
-            
-            _endpoints.Add(new Relay(Id,"Relay1", _controller, 17));
-            _endpoints.Add(new Relay(Id,"Relay2", _controller, 27));
-            _endpoints.Add(new Relay(Id,"Relay3", _controller, 22));
-            _endpoints.Add(new Relay(Id,"Relay4", _controller, 23));
-            
+
+            for (var index = 0; index < _relayPins.Length; index++)
+            {
+                _endpoints.Add(new Relay(Id, $"Relay{index + 1}", _controller, _relayPins[index]));
+                _controller.RegisterCallbackForPinValueChangedEvent(_relayPins[index],
+                    PinEventTypes.Falling | PinEventTypes.Rising, PinStateChangedCallback);
+            }
+
             OnUpdatedEndpoints();
+        }
+
+        private void PinStateChangedCallback(object sender, PinValueChangedEventArgs eventArgs)
+        {
+            OnStateChanged(_endpoints.Single(endpoint => endpoint.Id == eventArgs.PinNumber.ToString()),
+                eventArgs.ChangeType == PinEventTypes.Rising);
         }
 
         public void Unload()
         {
+            foreach (int relayPin in _relayPins)
+            {
+                _controller.UnregisterCallbackForPinValueChangedEvent(relayPin, PinStateChangedCallback);
+            }
+            
             _endpoints.Clear();
                 
             _controller?.Dispose();
@@ -68,9 +83,9 @@ namespace Aporta.Drivers.IonoPi
             OnlineStatusChanged?.Invoke(this, e);
         }
 
-        protected virtual void OnStateChanged(StateChangedEventArgs e)
+        protected virtual void OnStateChanged(IEndpoint endpoint, bool state)
         {
-            StateChanged?.Invoke(this, e);
+            StateChanged?.Invoke(this, new StateChangedEventArgs(endpoint, state));
         }
 
         protected virtual void OnAccessCredentialReceived(AccessCredentialReceivedEventArgs e)
