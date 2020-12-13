92d18cf6ec39a801374ffae3735a76205d092d25
Jonathan Horvath <byteme@bytedreamer.com>
2020-12-12 19:49:45 -0500

    Remove device reset logic

--- src/Drivers/Aporta.Drivers.OSDP/OSDPDriver.cs
+++ src/Drivers/Aporta.Drivers.OSDP/OSDPDriver.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Aporta.Extensions.Endpoint;
 using Aporta.Extensions.Hardware;
@@ -27,7 +26,6 @@ namespace Aporta.Drivers.OSDP
         private ControlPanel _panel;
         private ILogger<OSDPDriver> _logger;
         private Configuration _configuration = new Configuration {Buses = new List<Bus>()};
-        private Timer _offlineTimer;
 
         public Guid Id => Guid.Parse("D3C5DE68-E019-48D6-AB58-76F4B15CD0D5");
 
@@ -51,25 +49,6 @@ namespace Aporta.Drivers.OSDP
             AddEndpoints();
 
             AddDevices();
-            
-            _offlineTimer = new Timer(ResetOfflineDevices);
-            _offlineTimer.Change(TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(10));
-        }
-
-        private void ResetOfflineDevices(object state)
-        {
-            foreach (var bus in _configuration.Buses)
-            {
-                foreach (var offlineDevice in bus.Devices.Where(device =>
-                    !_panel.IsOnline(_portMapping[bus.PortName], device.Address)))
-                {
-                    _logger.LogInformation($"Attempting to reset offline device {offlineDevice.Name}");
-                    _panel.RemoveDevice(_portMapping[bus.PortName], offlineDevice.Address);
-                    Thread.Sleep(TimeSpan.FromSeconds(3));
-                    _panel.AddDevice(_portMapping[bus.PortName], offlineDevice.Address, true,
-                        offlineDevice.RequireSecurity);
-                }
-            }
         }
 
         private void AddEndpoints()
@@ -281,8 +260,6 @@ namespace Aporta.Drivers.OSDP
 
         public void Unload()
         {
-            _offlineTimer.Dispose();
-            
             _panel.Shutdown();
             
             _panel.ConnectionStatusChanged -= PanelOnConnectionStatusChanged;
