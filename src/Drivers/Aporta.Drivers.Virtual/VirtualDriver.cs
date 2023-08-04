42ae0d5ace48712d2ecef52d8002710f5b83fee1
Jonathan Horvath <qubit@z-bitco.com>
2023-08-04 05:31:39 -0400

    Add empty virtual driver

---
+++ src/Drivers/Aporta.Drivers.Virtual/VirtualDriver.cs
@@ -0,0 +1,38 @@
+﻿using Aporta.Extensions.Endpoint;
+using Aporta.Extensions.Hardware;
+using Microsoft.Extensions.Logging;
+
+namespace Aporta.Drivers.Virtual;
+
+public class VirtualDriver : IHardwareDriver
+{
+    private readonly List<IEndpoint> _endpoints = new();
+    
+    public string Name => "Virtual";
+    public Guid Id  => Guid.Parse("6667E442-53B2-4240-A10D-25F5E4400D83");
+    public IEnumerable<IEndpoint> Endpoints => _endpoints;
+    public void Load(string configuration, ILoggerFactory loggerFactory)
+    {
+
+    }
+
+    public void Unload()
+    {
+
+    }
+
+    public string CurrentConfiguration()
+    {
+        return string.Empty;
+    }
+
+    public Task<string> PerformAction(string action, string parameters)
+    {
+        return Task.FromResult(string.Empty);
+    }
+
+    public event EventHandler<EventArgs>? UpdatedEndpoints;
+    public event EventHandler<AccessCredentialReceivedEventArgs>? AccessCredentialReceived;
+    public event EventHandler<StateChangedEventArgs>? StateChanged;
+    public event EventHandler<OnlineStatusChangedEventArgs>? OnlineStatusChanged;
+}
