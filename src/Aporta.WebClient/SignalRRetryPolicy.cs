562db7346c5cc79762e9ab50f632855f8688e20a
Jonathan Horvath <qubit@z-bitco.com>
2023-09-11 16:32:37 -0400

    Continue connection to SignalR forever

---
+++ src/Aporta.WebClient/SignalRRetryPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Aporta.Shared;
+
+public class SignalRRetryPolicy : IRetryPolicy
+{
+    public TimeSpan? NextRetryDelay(RetryContext retryContext)
+    {
+        return retryContext.PreviousRetryCount switch
+        {
+            < 5 => TimeSpan.FromSeconds(3),
+            < 20 => TimeSpan.FromSeconds(10),
+            _ => TimeSpan.FromSeconds(30)
+        };
+    }
+}
