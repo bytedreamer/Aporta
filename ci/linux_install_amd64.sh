979dd6c60212305859da9dd1b1b72d6b15d41866
Jonathan Horvath <qubit@z-bitco.com>
2023-09-18 14:59:17 -0400

    Fix copying of files during deb creation

--- ci/linux_install_amd64.sh
+++ ci/linux_install_amd64.sh
@@ -4,6 +4,6 @@ mkdir -p "$1/package/amd64/DEBIAN"
 mkdir -p "$1/package/amd64/opt/Aporta"
 cp "$1/setup/Linux/control-amd64" "$1/package/amd64/DEBIAN/control"
 
-cp -R "$1/src/Aporta/bin/Release/net7.0/linux-x64/publish/*" "$1/package/amd64/opt/Aporta"
+cp -RT "$1/src/Aporta/bin/Release/net7.0/linux-x64/publish/" "$1/package/amd64/opt/Aporta/"
 
-dpkg-deb -b "$1/package/amd64" -z gzip "$2/Aporta.linux-amd64.deb"
+dpkg-deb -b "$1/package/amd64" -Zgzip "$2/Aporta.linux-amd64.deb"
--- ci/linux_install_armhf.sh
+++ ci/linux_install_armhf.sh
@@ -4,6 +4,6 @@ mkdir -p "$1/package/armhf/DEBIAN"
 mkdir -p "$1/package/armhf/opt/Aporta"
 cp "$1/setup/Linux/control-armhf" "$1/package/armhf/DEBIAN/control"
 
-cp -R "$1/src/Aporta/bin/Release/net7.0/linux-arm/publish/*" "$1/package/armhf/opt/Aporta"
+cp -RT "$1/src/Aporta/bin/Release/net7.0/linux-arm/publish/" "$1/package/armhf/opt/Aporta/"
 
-dpkg-deb -b "$1/package/armhf" -z gzip "$2/Aporta.linux-armhf.deb"
+dpkg-deb -b "$1/package/armhf" -Zgzip "$2/Aporta.linux-armhf.deb"
