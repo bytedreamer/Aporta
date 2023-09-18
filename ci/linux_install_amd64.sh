80feec432b1947abef859bb1552d892c6071396b
Jonathan Horvath <qubit@z-bitco.com>
2023-09-17 22:09:57 -0400

    Test

--- ci/linux_install_amd64.sh
+++ ci/linux_install_amd64.sh
@@ -4,6 +4,9 @@ mkdir -p "$1/package/amd64/DEBIAN"
 mkdir -p "$1/package/amd64/usr/local/bin/Aporta"
 cp "$1/setup/Linux/control-amd64" "$1/package/amd64/DEBIAN/control"
 
+echo "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish"
+ls "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish"
+
 cp -r "$1/src/Aporta/bin/Release/net7.0/linux-x64/publish/*" "$1/package/amd64/usr/local/bin/Aporta"
 
 dpkg-deb --build "$1/package/amd64" "$2/Aporta.linux-amd64.deb"
