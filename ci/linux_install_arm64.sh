348b6c4a37e0f27b5a61485350333f6f36ade032
Jonathan Horvath <qubit@z-bitco.com>
2023-09-17 22:13:14 -0400

    Try to properly copy files for deb package

--- ci/linux_install_arm64.sh
+++ ci/linux_install_arm64.sh
@@ -4,6 +4,6 @@ mkdir -p "$1/package/arm64/DEBIAN"
 mkdir -p "$1/package/arm64/usr/local/bin/Aporta"
 cp "$1/setup/Linux/control-arm64" "$1/package/arm64/DEBIAN/control"
 
-cp -r "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish/*" "$1/package/arm64/usr/local/bin/Aporta"
+cp -R "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish" "$1/package/arm64/usr/local/bin/Aporta"
 
 dpkg-deb --build "$1/package/arm64" "$2/Aporta.linux-arm64.deb"
