dbf06da6b21a5d88584fe208f2268916076a79ae
Jonathan Horvath <qubit@z-bitco.com>
2023-09-18 07:59:17 -0400

    Update documentation and install file locations

--- ci/linux_install_amd64.sh
+++ ci/linux_install_amd64.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p "$1/package/amd64/DEBIAN"
-mkdir -p "$1/package/amd64/usr/local/bin/Aporta"
+mkdir -p "$1/package/amd64/opt/Aporta"
 cp "$1/setup/Linux/control-amd64" "$1/package/amd64/DEBIAN/control"
 
-cp -R "$1/src/Aporta/bin/Release/net7.0/linux-x64/publish/*" "$1/package/amd64/usr/local/bin/Aporta"
+cp -R "$1/src/Aporta/bin/Release/net7.0/linux-x64/publish/*" "$1/package/amd64/opt/Aporta"
 
 dpkg-deb --build "$1/package/amd64" "$2/Aporta.linux-amd64.deb"
--- ci/linux_install_arm64.sh
+++ ci/linux_install_arm64.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 mkdir -p "$1/package/arm64/DEBIAN"
-mkdir -p "$1/package/arm64/usr/local/bin/Aporta"
+mkdir -p "$1/package/arm64/opt/Aporta"
 cp "$1/setup/Linux/control-arm64" "$1/package/arm64/DEBIAN/control"
 
-cp -R "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish/*" "$1/package/arm64/usr/local/bin/Aporta"
+cp -R "$1/src/Aporta/bin/Release/net7.0/linux-arm64/publish/*" "$1/package/arm64/opt/Aporta"
 
 dpkg-deb --build "$1/package/arm64" "$2/Aporta.linux-arm64.deb"
