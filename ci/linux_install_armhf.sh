f28e0983baa1b21a297d136ad1973d627d692a3e
Jonathan Horvath <qubit@z-bitco.com>
2023-09-17 22:13:14 -0400

    Try to properly copy files for deb package

--- ci/linux_install_armhf.sh
+++ ci/linux_install_armhf.sh
@@ -4,6 +4,6 @@ mkdir -p "$1/package/armhf/DEBIAN"
 mkdir -p "$1/package/armhf/usr/local/bin/Aporta"
 cp "$1/setup/Linux/control-armhf" "$1/package/armhf/DEBIAN/control"
 
-cp -r "$1/src/Aporta/bin/Release/net7.0/linux-arm/publish/*" "$1/package/armhf/usr/local/bin/Aporta"
+cp -R "$1/src/Aporta/bin/Release/net7.0/linux-arm/publish" "$1/package/armhf/usr/local/bin/Aporta"
 
 dpkg-deb --build "$1/package/armhf" "$2/Aporta.linux-armhf.deb"
