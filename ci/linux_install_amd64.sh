df496e90b25c1868a5943c6c271e525e9be95fdd
Jonathan Horvath <qubit@z-bitco.com>
2023-09-17 20:57:07 -0400

    Save artifacts for amd64

--- ci/linux_install_amd64.sh
+++ ci/linux_install_amd64.sh
@@ -6,4 +6,4 @@ cp setup/Linux/control-amd64 package/amd64/DEBIAN/control
 
 cp -r src/Aporta/bin/Release/net7.0/linux-x64/publish/* package/amd64/usr/local/bin/Aporta
 
-dpkg-deb --build package/amd64 package/output/Aporta_amd64.deb
+dpkg-deb --build package/amd64 "$1/Aporta_amd64.deb"
