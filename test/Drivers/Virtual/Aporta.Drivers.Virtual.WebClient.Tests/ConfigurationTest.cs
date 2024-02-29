2f13323f945549612e41e28a88e57196463038f5
Jonathan Horvath <qubit@z-bitco.com>
2024-02-29 10:24:27 -0500

    Add basic test project for virtual driver web client

---
+++ test/Drivers/Virtual/Aporta.Drivers.Virtual.WebClient.Tests/ConfigurationTest.cs
@@ -0,0 +1,30 @@
+using Bunit;
+using TestWebClientConfiguration;
+
+namespace Aporta.Drivers.Virtual.WebClient.Tests;
+
+[FixtureLifeCycle(LifeCycle.InstancePerTestCase)]
+public class ConfigurationTest : AportaTestContext
+{
+    private const string EmptyConfiguration = "{\"Readers\":[],\"Outputs\":[],\"Inputs\":[]}";
+    
+    private IRenderedComponent<Configuration>? _cut;
+    
+    public ConfigurationTest()
+    {
+        BlazoriseConfig.JSInterop.AddTextEdit(JSInterop);
+        BlazoriseConfig.JSInterop.AddButton(JSInterop);
+    }
+
+    [Test]
+    public void ConfigurationComponentRendersCorrectly()
+    {
+        // Act - Render with an empty configuration
+        _cut = RenderComponent<Configuration>(parameters => parameters
+            .Add(p => p.RawConfiguration, EmptyConfiguration));
+
+        // Assert
+        Assert.That(_cut.Nodes[0].TextContent, Is.EqualTo("Setup"));
+    }
+  
+}
