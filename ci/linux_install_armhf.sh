8a7a0b1303524be772680039030d23b984edb9c2
Jonathan Horvath <qubit@z-bitco.com>
2024-03-24 13:12:01 -0400

    Fix linux builds

--- ci/linux_install_armhf.sh
+++ ci/linux_install_armhf.sh
@@ -4,6 +4,6 @@ mkdir -p "$1/package/armhf/DEBIAN"
 mkdir -p "$1/package/armhf/opt/Aporta"
 cp "$1/setup/Linux/control-armhf" "$1/package/armhf/DEBIAN/control"
 
-cp -RT "$1/src/Aporta/bin/Release/net7.0/linux-arm/publish/" "$1/package/armhf/opt/Aporta/"
+cp -RT "$1/src/Aporta/bin/Release/net8.0/linux-arm/publish/" "$1/package/armhf/opt/Aporta/"
 
 dpkg-deb -Zgzip -b "$1/package/armhf" "$2/Aporta.linux-armhf.deb"
