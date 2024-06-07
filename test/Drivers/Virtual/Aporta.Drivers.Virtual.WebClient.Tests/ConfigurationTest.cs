648fbbd10e6907b872f9cb4fe96f16d0b8fe5693
Chris Lind <chrislind@classical-computer.com>
2024-06-07 16:45:07 -0400

    Created a "Swipe Badge" unit test for the Virtual Device Driver

--- test/Drivers/Virtual/Aporta.Drivers.Virtual.WebClient.Tests/ConfigurationTest.cs
+++ test/Drivers/Virtual/Aporta.Drivers.Virtual.WebClient.Tests/ConfigurationTest.cs
@@ -1,8 +1,14 @@
+using Aporta.Drivers.Virtual.Shared.Actions;
+using Aporta.Drivers.Virtual.Shared;
 using Aporta.Shared.Calls;
+using Blazorise;
 using Bunit;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using Newtonsoft.Json;
 using TestWebClientConfiguration;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using AngleSharp.Diffing.Extensions;
 
 namespace Aporta.Drivers.Virtual.WebClient.Tests;
 
@@ -10,17 +16,63 @@ namespace Aporta.Drivers.Virtual.WebClient.Tests;
 public class ConfigurationTest : AportaTestContext
 {
     private const string EmptyConfiguration = "{\"Readers\":[],\"Outputs\":[],\"Inputs\":[]}";
-    
+
     private readonly Mock<IDriverConfigurationCalls> _mockConfigurationCalls = new();
-    
+
+    private readonly Guid _extensionId = Guid.NewGuid();
+
     private IRenderedComponent<Configuration>? _cut;
     
     public ConfigurationTest()
     {
         BlazoriseConfig.JSInterop.AddTextEdit(JSInterop);
         BlazoriseConfig.JSInterop.AddButton(JSInterop);
-        
+
         Services.AddScoped<IDriverConfigurationCalls>(_ => _mockConfigurationCalls.Object);
+    }
+
+    private Shared.Configuration SetUpDeviceConfiguration(byte readerNumber)
+    {
+        var config = new Shared.Configuration();
+
+        config.Readers.Add(new Reader { Name = "Virtual Reader 1", Number = readerNumber });
+        config.Outputs.Add(new Output { Name = "Virtual Output 1", Number = readerNumber });
+        config.Inputs.Add(new Input { Name = "Virtual Input 1", Number = readerNumber });
+
+        return config;
+    }
+
+    [Test]
+    public async Task SwipeBadge()
+    {
+        // Arrange
+        byte readerNumber = 1;
+        var cardData = "2468";
+        var testConfiguration = JsonConvert.SerializeObject(SetUpDeviceConfiguration(readerNumber));
+
+        Dictionary<byte, string> badgeNumbers = new Dictionary<byte, string>() {
+            {readerNumber, cardData }
+         };
+
+        string badgeSwipeParameters = JsonConvert.SerializeObject(new BadgeSwipeAction
+        {            
+            ReaderNumber = readerNumber,
+            CardData = badgeNumbers[readerNumber]
+            
+        });
+
+        // Act
+        _cut = RenderComponent<Configuration>(parameters => parameters
+            .Add(p => p.RawConfiguration, testConfiguration)
+            .Add(p => p.ExtensionId, _extensionId)
+            .Add(p => p.BadgeNumbers, badgeNumbers));
+
+        var badgeSwipeButton = _cut.FindComponents<Button>().First(button => button.Nodes[0].TextContent.Trim() == "Click to Simulate Badge Swipe");
+
+        await _cut.InvokeAsync(async () => await badgeSwipeButton.Instance.Clicked.InvokeAsync());
+
+        // Assert
+        _mockConfigurationCalls.Verify(calls => calls.PerformAction(_extensionId, ActionType.BadgeSwipe.ToString(), badgeSwipeParameters));
     }
 
     [Test]
@@ -29,6 +81,10 @@ public class ConfigurationTest : AportaTestContext
         // Act - Render with an empty configuration
         _cut = RenderComponent<Configuration>(parameters => parameters
             .Add(p => p.RawConfiguration, EmptyConfiguration));
+
+        _cut = RenderComponent<Configuration>(parameters => parameters
+            .Add(p => p.RawConfiguration, EmptyConfiguration)
+            .Add(p => p.ExtensionId, _extensionId));
 
         // Assert
         Assert.That(_cut.Nodes[0].TextContent, Does.Not.Contain("Setup"));
